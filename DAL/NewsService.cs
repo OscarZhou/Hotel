9c4eb6e6d3cbc86d6b6fafb224c7096a33e7a9c0
OscarZhou <zhouhongyu1989@gmail.com>
2017-09-18 18:52:52 +1200

    DAL and BLL for News object 1. query definite data 2. insert the news 3. obtain the detail of the news (query a news detail and return object by querying NewsId) 4. query all news information (latest published and paging) 5. modify the news 6. delete the news

---
+++ DAL/NewsService.cs
@@ -0,0 +1,159 @@
+﻿
+using DAL.DBHelper;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class NewsService
+    {
+        /// <summary>
+        /// Query data by specifying definite number
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public List<News> GetNews(int count)
+        {
+            string sql = "SELECT TOP @COUNT NewsId, NewsTitle, NewsContents, PublishTime, CategoryId, CategoryName FROM [dbo].[News] INNER JOIN [dbo].[NewsCategory] ON NewsCateogry.CategoryId = News.CategoryId ORDER BY PublishTime DESC";
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@COUNT", count)
+            };
+
+            List<News> list = new List<News>();
+            SqlDataReader objReader = SQLHelper.GetReader(sql, param);
+            while (objReader.Read())
+            {
+                list.Add(new News()
+                {
+                    CategoryId = Convert.ToInt32( objReader["CategoryId"]),
+                    NewsContents = objReader["NewsContents"].ToString(),
+                    NewsId = Convert.ToInt32(objReader["NewsId"]),
+                    NewsTitle = objReader["NewsTitle"].ToString(),
+                    PublishTime = Convert.ToDateTime(objReader["PublishTime"]),
+                    CategoryName = objReader["CategoryName"].ToString()
+                });
+            }
+
+            objReader.Close();
+            return list;
+        }
+
+        /// <summary>
+        /// Publish News
+        /// </summary>
+        /// <param name="objNews"></param>
+        /// <returns></returns>
+        public int PublishNews(News objNews)
+        {
+            string sql =
+                "INSERT INTO [dbo].[News] (NewsTitle, NewsContents, CategoryId) VALUES(@NewTitle, @NewsContent, @CategoryId)";
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@NewsTitle", objNews.NewsTitle), 
+                new SqlParameter("@NewsContents", objNews.NewsContents),
+                new SqlParameter("@CategoryId", objNews.CategoryId) 
+            };
+
+            int result = SQLHelper.Update(sql, param);
+            return result;
+        }
+
+        /// <summary>
+        /// Get News detail by NewsId
+        /// </summary>
+        /// <param name="newsId"></param>
+        /// <returns></returns>
+        public News GetNewsById(string newsId)
+        {
+            string sql = "SELECT NewsId, NewsTitle, NewsContents, CategoryId, PublishTime FROM [dbo].[News] WHERE NewsId = @NewsId";
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@NewsId", newsId) 
+            };
+            News objNews = null;
+            SqlDataReader objReader = SQLHelper.GetReader(sql, param);
+            if (objReader.Read())
+            {
+                objNews = new News()
+                {
+                    NewsId = Convert.ToInt32(objReader["NewsId"]),
+                    NewsTitle = objReader["NewsTitle"].ToString(),
+                    NewsContents = objReader["NewsContents"].ToString(),
+                    CategoryId = Convert.ToInt32(objReader["CategoryId"]),
+                    PublishTime = Convert.ToDateTime(objReader["PublishTime"])
+                };
+                
+            }
+            objReader.Close();
+            return objNews;
+        }
+
+        /// <summary>
+        /// Query all News
+        /// </summary>
+        /// <returns></returns>
+        public List<News> GetNews()
+        {
+            string sql = "SELECT NewsId, NewsTitle, NewsContents, PublishTime, CategoryId, CategoryName FROM [dbo].[News] INNER JOIN [dbo].[NewsCategory] ON NewsCateogry.CategoryId = News.CategoryId ORDER BY PublishTime DESC";
+
+            List<News> list = new List<News>();
+            SqlDataReader objReader = SQLHelper.GetReader(sql);
+            while (objReader.Read())
+            {
+                list.Add(new News()
+                {
+                    CategoryId = Convert.ToInt32( objReader["CategoryId"]),
+                    NewsContents = objReader["NewsContents"].ToString(),
+                    NewsId = Convert.ToInt32(objReader["NewsId"]),
+                    NewsTitle = objReader["NewsTitle"].ToString(),
+                    PublishTime = Convert.ToDateTime(objReader["PublishTime"]),
+                    CategoryName = objReader["CategoryName"].ToString()
+                });
+            }
+
+            objReader.Close();
+            return list;
+        }
+
+
+        /// <summary>
+        /// Modify News
+        /// </summary>
+        /// <param name="objNews"></param>
+        /// <returns></returns>
+        public int ModifyNews(News objNews)
+        {
+            string sql =
+                "Update [dbo].[News] SET NewsTitle = @NewsTitle, NewsContents = @NewsContents, CategoryId = @CategoryId";
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@NewsTitle", objNews.NewsTitle),
+                new SqlParameter("@NewsContents", objNews.NewsContents),
+                new SqlParameter("@CategoryId", objNews.CategoryId) 
+            };
+
+            int result = SQLHelper.Update(sql, param);
+            return result;
+        }
+
+        /// <summary>
+        /// Delete News
+        /// </summary>
+        /// <param name="objNews"></param>
+        /// <returns></returns>
+        public int DeleteNews(News objNews)
+        {
+            string sql = "DELETE FROM [dbo].[News] WHERE NewsId = @NewsId";
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@NewsId", objNews.NewsId) 
+            };
+
+            int result = SQLHelper.Update(sql, param);
+            return result;
+        }
+    }
+}
---
+++ Models/News.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Models
+{
+    public class News
+    {
+        public int NewsId { get; set; }
+
+        public string NewsTitle { get; set; }
+
+        public string NewsContents { get; set; }
+
+        public DateTime PublishTime { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+    }
+}
