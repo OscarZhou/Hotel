dcb04fb197964411600ed3b4f8798fb4264824fc
OscarZhou <zhouhongyu1989@gmail.com>
2017-09-19 12:18:05 +1200

    BLL, DAL and Models for SysAdmin object 1. login query according to user name and password

---
+++ DAL/SysAdminService.cs
@@ -0,0 +1,36 @@
+﻿
+using DAL.DBHelper;
+using Models;
+using System;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class SysAdminService
+    {
+        public SysAdmin Login(SysAdmin objAdmin)
+        {
+            string sql =
+                "SELECT LoginId, LoginPwd, LoginName FROM [dbo].[SysAdmins] WHERE LoginId = @LoginId and LoginPwd = @LoginPwd";
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@LoginId", objAdmin.LoginId), 
+                new SqlParameter("@LoginPwd", objAdmin.LoginPwd)
+            };
+
+            SysAdmin objRetAdmin = null;
+            SqlDataReader objReader = SQLHelper.GetReader(sql, param);
+            if (objReader.Read())
+            {
+                objRetAdmin = new SysAdmin()
+                {
+                    LoginId = Convert.ToInt32(objReader["LoginId"]),
+                    LoginPwd = objReader["LoginPwd"].ToString(),
+                    LoginName = objReader["LoginName"].ToString()
+                };
+            }
+            objReader.Close();
+            return objRetAdmin;
+         }
+    }
+}
---
+++ Models/SysAdmin.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Models
+{
+    public class SysAdmin
+    {
+        public int LoginId { get; set; }
+
+        public string LoginPwd { get; set; }
+
+        public string LoginName { get; set; }
+
+    }
+}
