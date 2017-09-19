e19b29c3a8bb4667baced3a113f7e5ebd530fafc
OscarZhou <zhouhongyu1989@gmail.com>
2017-09-19 12:20:33 +1200

    Add SysAdmin BLL

---
+++ BLL/SysAdminManager.cs
@@ -0,0 +1,15 @@
+﻿using DAL;
+using Models;
+
+namespace BLL
+{
+    public class SysAdminManager
+    {
+        private SysAdminService objSysAdminService = new SysAdminService();
+
+        public SysAdmin Login(SysAdmin objAdmin)
+        {
+            return objSysAdminService.Login(objAdmin);
+        }
+    }
+}
