5f4c24046fb33ecc776564e2ca85d2a971bae10b
OscarZhou <zhouhongyu1989@gmail.com>
2017-09-21 23:11:40 +1200

    1. Add admin part 2. modify the layout page

---
+++ HotelPro/Areas/HotelAdmin/Controllers/SysAdminController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace HotelPro.Areas.HotelAdmin.Controllers
+{
+    public class SysAdminController : Controller
+    {
+        //
+        // GET: /HotelAdmin/SysAdmin/
+
+        public ActionResult Index()
+        {
+            return View("AdminMain");
+        }
+
+    }
+}
